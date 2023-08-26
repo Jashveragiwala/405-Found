4aaae419637941916e4c5c7cd4b45c93517ae85b
Jashveragiwala <66709554+Jashveragiwala@users.noreply.github.com>
2023-08-26 23:02:19 +0800

    storage

--- src/routes/signup/storage.js
+++ src/routes/signup/storage.js
@@ -3,17 +3,29 @@
 // Load existing user data from localStorage or initialize an empty array
 // localStorage.setItem('users', JSON.stringify([]));  
 // const users = JSON.parse(localStorage.getItem('users') || '[]');
-var users = []
+// var users = []
+// Try running this in a clean HTML file
+
+// localStorage.setItem('test', 'Hello, localStorage!');
+
+// const storedValue = localStorage.getItem('test');
+// console.log(storedValue); // Should output: "Hello, localStorage!"
+
 
 export function addUser(user) {
-  users.push(user);
-//   localStorage.setItem('users', JSON.stringify(users));
+    // users.push(user);
+    // console.log(users);
+    const jsonObject = JSON.stringify(user);
+    localStorage.setItem('users',JSON.parse(jsonObject));
+    console.log(localStorage.getItem('users'));
 }
 
 export function verifyUser(username, password) {
   // Loop through the users array and check for a matching user
+  const users = localStorage.getItem('users');
+  console.log(users[0]);
   for (const user of users) {
-    if (user.email === username && user.password === password) {
+    if (user.email == username && user.password == password) {
       return true;
     }
   }
